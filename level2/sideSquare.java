aa6eebbdbd4db1a18bdf59c84da1bd5105df6476
jinnykapur <jinnykapur@gmail.com>
2025-08-21 03:24:39 +0530

    Add level2 folder

---
+++ level2/sideSquare.java
@@ -0,0 +1,17 @@
+/*
+Write a program to find the side of the square whose parameter you read from the user 
+Hint => Perimeter of the Square is 4 times the side
+I/P => perimeter
+O/P => The length of the side is ___ whose perimeter is ____
+*/
+import java.util.*;
+
+public class sideSquare{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the side of square: ");
+		int s = sc.nextInt();
+		int perimeter=s*4;
+		System.out.println("The length of the side is "+s+" cm whose perimeter is "+perimeter+" cm.");
+	}
+}
